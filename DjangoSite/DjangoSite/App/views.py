c1c1dac41c892a598f534ce472c3716180ffff6e
codeCollision4 <79557266+codeCollision4@users.noreply.github.com>
2021-11-13 16:04:53 -0600

    Images can now be uploaded and saved to media file

--- DjangoSite/DjangoSite/App/views.py
+++ DjangoSite/DjangoSite/App/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
+from .forms import ImageForm
+from .models import ImageModel
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def home(request):
     return render(request, 'App/home.html')
 
 def upload(request):
-    return HttpResponse("""<h1>Upload Home</h1>""")
+    if request.method == 'POST':
+        uploaded_image = request.FILES['Image']
+        fs = FileSystemStorage()
+        fs.save(uploaded_image.name, uploaded_image)
+    return render(request, 'App/upload.html')
 
 def result(request):
     return HttpResponse("""<h1>Results Home</h1>""")
--- DjangoSite/DjangoSite/DjangoSite/urls.py
+++ DjangoSite/DjangoSite/DjangoSite/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #maps urls to our main app, only need one per app, will link to url.py in app directory
     path('home/', include('App.urls')),
 ]
+
+#Only for dev time
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
