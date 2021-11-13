6ee00044498d4c6d3506dda8641bee73230df279
codeCollision4 <79557266+codeCollision4@users.noreply.github.com>
2021-11-13 14:01:02 -0600

    Added upload and results webpages for App
    Also added a placeholder html folder for upload will now try to add form

--- DjangoSite/DjangoSite/App/views.py
+++ DjangoSite/DjangoSite/App/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse, request
 # Create your views here.
 def home(request):
     return render(request, 'App/home.html')
+
+def upload(request):
+    return HttpResponse("""<h1>Upload Home</h1>""")
+
+def result(request):
+    return HttpResponse("""<h1>Results Home</h1>""")
