9701adbf2d5dc9b59f13179f58432fafd49efedd
rishixyz9 <reach2rishabh@rocketmail.com>
2021-11-13 14:22:16 -0600

    Revert "k2"
    This reverts commit 6c3a23bd5ef689288ebdc1eeb314ed3a82ea7f37.

--- DjangoSite/DjangoSite/App/urls.py
+++ DjangoSite/DjangoSite/App/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #home/
     path('', views.home, name='app-home'),
+    #home/upload
+    path('upload/', views.upload, name='app-upload'),
+    #home/results
+    path('results/', views.result, name='app-result')
 ]
