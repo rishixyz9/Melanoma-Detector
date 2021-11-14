4bc2d2a8b016e5ca57dd74d92a95af4eff9f3045
codeCollision4 <79557266+codeCollision4@users.noreply.github.com>
2021-11-14 03:39:17 -0600

    Update process to img/255

--- DjangoSite/Tools/process.py
+++ DjangoSite/Tools/process.py
@@ -12,7 +12,7 @@ def process(image_path):
     img = image.load_img(image_path, target_size=(224, 224))
     plt.imshow(img)
     img = np.expand_dims(img, axis=0)
-
+    img = img/255
     result=loaded_model.predict(img)
     obj = [result[0], image_path]
     return(obj)
@@ -23,7 +23,7 @@ class init_model:
     def process(self, img):
         img.resize((224,224))
         img = np.expand_dims(img, axis=0)
-
+        img = img/255
         result = self.loaded_model.predict(img)
         return result[0] # the chance that it's melanoma
 
