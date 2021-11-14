ff2cee8906d4bf8781544454ee9e4ac7d5a0b99f
rishixyz9 <reach2rishabh@rocketmail.com>
2021-11-13 19:35:47 -0600

    23

--- DjangoSite/App/views.py
+++ DjangoSite/App/views.py
@@ -3,16 +3,25 @@ from django.http import HttpResponse, request
 from .forms import ImageForm
 from .models import ImageModel
 from django.core.files.storage import FileSystemStorage
+from Tools.process import process
 
 # Create your views here.
 def home(request):
-    return render(request, 'App/home.html')
+    return render (request, 'App/home.html')
+
+def resultScreen(request, arg):
+    result = process(arg['img_url'])
+    arg['result'] = result[0]
+    return render(request, 'App/result.html', arg)
 
 def upload(request):
+    context={}
     if request.method == 'POST':
         uploaded_image = request.FILES['Image']
         fs = FileSystemStorage()
-        fs.save(uploaded_image.name, uploaded_image)
+        name = fs.save(uploaded_image.name, uploaded_image)
+        context['img_url']=fs.url(name)
+        return resultScreen(request, context)
     return render(request, 'App/upload.html')
 
 def result(request):
