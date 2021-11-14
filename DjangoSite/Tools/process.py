d0cd631ca0740212ddeb069feca82bb579223bc4
rishixyz9 <reach2rishabh@rocketmail.com>
2021-11-13 22:34:44 -0600

    hi

--- DjangoSite/Tools/process.py
+++ DjangoSite/Tools/process.py
@@ -5,9 +5,9 @@ import numpy as np
 import tensorflow as tf
 
 def process(image_path):
-    image_path = './' + image_path
+    image_path = '.' + image_path
+    print(image_path)
     loaded_model = tf.keras.models.load_model('ML_Model/MelanomaFinal.h5')
-    loaded_model.layers[0].input_shape
     img = image.load_img(image_path, target_size=(224, 224))
     plt.imshow(img)
     img = np.expand_dims(img, axis=0)
